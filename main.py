1874255f6acc76dfdd392738ab8cd46b684889d5
MakCimka <poduskapuska@gmail.com>
2024-11-27 10:54:10 +0300

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 stroka = input("Введите строку: ")
 stroka1 = input("Введите строку: ")
 stroka2 = input("Введите строку: ")
-if len(stroka) == 0 or len(stroka1) == 0 or len(stroka2) == 0:
+if len(stroka) == 0 or len(stroka1) == 0 or len(stroka2) == 0: #Сравниваем строки
     print("Строка введена неверно")
-else:
+else: #Выводим их количество
     print (len(stroka.split()))
     print (len(stroka1.split()))
     print (len(stroka2.split()))
